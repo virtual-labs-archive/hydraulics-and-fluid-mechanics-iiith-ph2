9683d11f74cc7d55f926c24edc04433dd3d9308c
M Amrutha Varshini <mandalaamruthavarshinistar@gmail.com>
2019-06-05 23:38:59 +0530

    Convert venturi meter experiment from java3D to javascript

--- src/lab/exp5/SRIP/Codes/main.js
+++ src/lab/exp5/SRIP/Codes/main.js
@@ -3,3 +3,32 @@
 
 
 
+function move() {
+  var elem = document.getElementById("myBar");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 60) {
+      clearInterval(id);
+    } else {
+      width=width+0.1; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+function move2() {
+
+
+  var elem = document.getElementById("myBar1");   
+  var width = 1;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 40) {
+      clearInterval(id);
+    } else {
+      width=width+0.1; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
+
