c5dd86b92381235dd6f59db4d82ff6b42e8a9298
M Amrutha Varshini <mandalaamruthavarshinistar@gmail.com>
2019-06-05 17:04:58 +0530

    Convert venturi meter experiment from java3D to javascript

--- src/lab/exp5/SRIP/Codes/main.js
+++ src/lab/exp5/SRIP/Codes/main.js
@@ -3,12 +3,3 @@
 
 
 
-window.onload = function() { var animateProgress=anime({
-  targets: 'progress',
-  value: 100,
-  easing: 'linear',
-  autoplay: false
-});
-document.querySelector('.play-progress1').onclick = animateProgress.restart;
-}
-
