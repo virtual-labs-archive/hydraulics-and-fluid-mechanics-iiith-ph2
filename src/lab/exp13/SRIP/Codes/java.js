c898c20cf20108dfcd36cae439122b692cac5df9
Jatin Kumar <17ucs186@lnmiit.ac.in>
2019-06-13 01:39:24 +0530

    task in progress

--- src/lab/exp13/SRIP/Codes/java.js
+++ src/lab/exp13/SRIP/Codes/java.js
@@ -36,13 +36,46 @@
 			var elem = document.getElementById('draw-shapes');
 			var two = new Two({ fullscreen: true }).appendTo(elem);
 
-			var rectA = two.makeRectangle(910, 600, 800, 800);
+			var rectA = two.makeRectangle(900, 400, 1500, 700);
 			rectA.stroke = 'black';
 
 			rectA.fill='grey';
 			rectA.opacity=0.10;
 
 			two.update();
+
+			// var d=SVG("draw-shapes").size(1500,1100);
+			// var rect=d.rect(1500,700).attr({
+			// 	'fill':'#000000',
+			// 	'stroke':'#000000',
+			// 	'stroke-width':'3px',
+			// 	'opacity':0.10
+			// });
+
+			// rect.x(300);
+			// rect.y(100);
+			
 		}
 
-		
+
+		function line()
+		{
+			var d=SVG("lines").size(800,800);
+			var l1=d.line([[480,500],[800,500]]).stroke({width:2})
+
+			var l2=d.line([[500,480],[800,480]]).stroke({width:2})
+
+			var l3=d.line([[480,300],[480,500]]).stroke({width:2})
+
+			var l4=d.line([[500,300],[500,480]]).stroke({width:2})
+
+			var l5=d.line([[500,300],[575,300]]).stroke({width:2})
+
+			var l6=d.line([[480,300],[400,300]]).stroke({width:2})
+
+			var l7=d.line([[400,300],[400,100]]).stroke({width:2})
+
+			var l8=d.line([[575,300],[575,100]]).stroke({width:2})
+
+		}
+
