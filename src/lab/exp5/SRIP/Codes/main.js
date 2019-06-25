a592a7148520212822e966c5fb52aa3a80b390fe
M Amrutha Varshini <mandalaamruthavarshinistar@gmail.com>
2019-06-25 19:20:54 +0530

    Venturi meter experiment #287

--- src/lab/exp5/SRIP/Codes/main.js
+++ src/lab/exp5/SRIP/Codes/main.js
@@ -5,16 +5,17 @@
 function move() {
   var elem = document.getElementById("myBar");   
   var width = 1;
-  var id = setInterval(frame, 10);
+  
   function frame() {
     if (width >= 57) {
       clearInterval(id);
 
     } else {
       width=width+0.05; 
-      elem.style.width = width + '%'; 
+      elem.style.width = width + "%"; 
     }
   }
+  var id = setInterval(frame, 10);
 }
 /*function move01() {
   var elem = document.getElementById("myBar");   
@@ -30,22 +31,23 @@ function move2() {
 
   var elem = document.getElementById("myBar1");   
   var width = 1;
-  var id = setInterval(frame, 10);
+
   function frame() {
     if (width >= 45) {
       clearInterval(id);
     } else {
       width=width+0.06; 
-      elem.style.width = width + '%'; 
+      elem.style.width = width + "%"; 
     }
   }
+  var id = setInterval(frame, 10);
 }
 function t1()
 {
 var path = document.querySelector('#top1');
 var length = path.getTotalLength();
 path.style.transition = path.style.WebkitTransition = 'none';
-path.style.strokeDasharray = length + ' ' + length;
+path.style.strokeDasharray = length + " " + length;
 path.style.strokeDashoffset = '0';
 
 
@@ -59,7 +61,7 @@ function t2()
 	var path2 = document.querySelector('#top2');
 var length2 = path2.getTotalLength();
 path2.style.transition = path2.style.WebkitTransition = 'none';
-path2.style.strokeDasharray = length2 + ' ' + length2;
+path2.style.strokeDasharray = length2 + " " + length2;
 path2.style.strokeDashoffset = '0';
 
 
@@ -73,7 +75,7 @@ path2.style.strokeDashoffset = length2;
 	var path3 = document.querySelector('#top3');
 var length3 = path3.getTotalLength();
 path3.style.transition = path3.style.WebkitTransition = 'none';
-path3.style.strokeDasharray = length3 + ' ' + length3;
+path3.style.strokeDasharray = length3 + " " + length3;
 path3.style.strokeDashoffset = '0';
 
 
@@ -86,7 +88,7 @@ function t4()
 	var path4 = document.querySelector('#top4');
 var length4 = path4.getTotalLength();
 path4.style.transition = path4.style.WebkitTransition = 'none';
-path4.style.strokeDasharray = length4 + ' ' + length4;
+path4.style.strokeDasharray = length4 + " " + length4;
 path4.style.strokeDashoffset = '0';
 
 
@@ -98,7 +100,7 @@ function t()
 	var path = document.querySelector('#top');
 var length = path.getTotalLength();
 path.style.transition = path.style.WebkitTransition = 'none';
-path.style.strokeDasharray = length + ' ' + length;
+path.style.strokeDasharray = length + " " + length;
 path.style.strokeDashoffset = '0';
 
 
